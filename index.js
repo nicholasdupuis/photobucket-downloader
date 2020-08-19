c27ae34d19e708fa72e90c59b951d4c7c520ed75
Nicholas Dupuis <ndupuis08@gmail.com>
2020-08-18 19:17:24 -0500

    Fix

--- index.js
+++ index.js
@@ -13,28 +13,16 @@ const download = (url, path, callback) => {
 
 fs.readFile('photobucket.txt', 'utf8', (err, urls) => {
     if (err) throw err;
-
-    // Build an array of filenames from photobucket.txt
     urls.split('\n').forEach(url => {
         fileNames.push(url.split('/').splice(-1)[0].replace('.html', '').replace('\r', ''));
     });
 
-    // Create images directory if one does not exist
-    fs.access('./images', (err) => {
-        if (err) {
-            fs.mkdir('./images', (err) => {
-                if (err) throw err;
-            });
-        }
-    });
-
-    // Download each file from Photobucket
     fileNames.forEach(file => {
-        const url = `https://abc123.photobucket.com/albums/a123/your_user_name/${file}`;
+        const url = `https://abc123.photobucket.com/albums/j243/user_name/${file}`;
         const path = `./images/${file}`;
 
         download(url, path, () => {
-            console.log(`${file} Downloaded!`)
+            console.log('Done!')
         });
     })
 });
